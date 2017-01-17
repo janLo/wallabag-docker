6ad4ba8ba58f609dd96dffb91779da40ea178a04
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-17 06:47:09 +0100

    Add ability to run pocket importer
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -4,5 +4,9 @@ if [ "$1" = "wallabag" ];then
     ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local
     exec s6-svscan /etc/s6/
 fi
+if [ "$1" = "import:pocket" ];then
+    ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local --skip-tags=firstrun
+    su -c "bin/console wallabag:import:redis-worker -e=prod pocket -vv" -s /bin/sh nobody
+fi
 
 exec "$@"
