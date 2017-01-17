378653eaef202a53932efb3715ea2799663cac19
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-01-17 15:44:53 +0100

    Seperate init phase from running
    If we have a special "init" phase, we do not need to leave the postgres
    password in the env for normal operation.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -8,8 +8,11 @@ provisioner () {
     echo "Provisioner finished."
 }
 
+if [ "$1" = "init" ];then
+    ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local
+fi
 if [ "$1" = "wallabag" ];then
-    provisioner
+    ansible-playbook -i /etc/ansible/hosts /etc/ansible/entrypoint.yml -c local --skip-tags=firstrun
     exec s6-svscan /etc/s6/
 fi
 if [ "$1" = "import" ];then
